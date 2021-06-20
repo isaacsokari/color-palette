9acb236ac2c00e8e1cfe7765fbff8478d28c37d8
Isaac Sokari <isaacsokari@gmail.com>
2021-06-20 22:29:52 +0100

    completed styling exercise

--- App.js
+++ App.js
@@ -5,7 +5,20 @@ export default function App() {
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={[styles.container, styles.teal]}>
-        <Text style={styles.text}>Hello World</Text>
+        <Text style={styles.text}>Here are some boxes of different colors</Text>
+
+        <View style={[styles.textContainer, styles.cyan]}>
+          <Text style={styles.text}>Cyan: #2aa198</Text>
+        </View>
+        <View style={[styles.textContainer, styles.blue]}>
+          <Text style={styles.text}>Blue: #268bd2 </Text>
+        </View>
+        <View style={[styles.textContainer, styles.magenta]}>
+          <Text style={styles.text}>Magenta: #d33682</Text>
+        </View>
+        <View style={[styles.textContainer, styles.orange]}>
+          <Text style={styles.text}>Orange: #cb4b16</Text>
+        </View>
       </View>
     </SafeAreaView>
   );
@@ -13,18 +26,36 @@ export default function App() {
 
 const styles = StyleSheet.create({
   safeArea: {
-    flex: 1
+    flex: 1,
   },
   teal: {
     backgroundColor: 'teal',
   },
   container: {
-    alignItems: 'center',
-    justifyContent: 'center',
     flex: 1,
+    justifyContent: 'center',
   },
   text: {
     color: '#fff',
     fontSize: 24,
+  },
+  textContainer: {
+    alignItems: 'center',
+    marginHorizontal: 10,
+    marginTop: 20,
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+  },
+  cyan: {
+    backgroundColor: '#2aa198',
+  },
+  blue: {
+    backgroundColor: '#268bd2',
+  },
+  magenta: {
+    backgroundColor: '#d33682',
+  },
+  orange: {
+    backgroundColor: '#cb4b16',
   },
 });
